ef41eea7c9c594a1af8035355ad765bc384f3408
Patraklos2000 <habetsgnl@gmail.com>
2023-05-24 04:49:38 +0200

    fesef
    efsefsef

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,190 @@
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+import {FC, useEffect, useReducer, useState} from "react";
+import {useDraw} from "@/hooks/useDraw";
+import {v4 as uuid} from 'uuid';
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+
+const initialNotesState ={
+  lastNoteCreated: null,
+  totalNotes: 0,
+  notes: [],
+}
+const notesReducer = (prevState, action) => {
+  switch (action.type){
+    case 'ADD_NOTE': {
+      const newState = {
+        lastNoteCreated: new Date().toTimeString().slice(0,8),
+        totalNotes: prevState.notes.length +1,
+        notes: [...prevState.notes, action.payload]
+      }
+      console.log('After ADD_NOTE: ', newState);
+      return newState;
+    }
+    case 'DELETE_NOTE': {
+      const newState = {
+        ...prevState,
+        totalNotes: prevState.notes.length -1,
+        notes: prevState.notes.filter(note => note.id !== action.payload.id),
+
+      }
+      console.log('after delete note ', newState)
+      return newState
+
+    }
+  }
+}
+interface pageProps {}
+
+
+
+
+const page: FC<pageProps> = ({}) => {
+  const [color, setColor] = useState<string>('#000000')
+  const [noteInput, setNoteInput] = useState('')
+  const [notesState, dispatch] = useReducer(notesReducer, initialNotesState)
+
+  const addNote = () => {
+    event.preventDefault()
+    if (!noteInput){
+      return;
+    }
+
+    const newNote = {
+      id: uuid(),
+      text: noteInput,
+    }
+
+    dispatch({type: 'ADD_NOTE', payload: newNote})
+
+  };
+  const {canvasRef, onMouseDown, clear} = useDraw(drawLine)
+
+  const exportPdf = () => {
+    const input = document.getElementById("App")
+    html2canvas(input, {logging: true, letterRendering: 1, useCORS: true}).then(canvas => {
+      const imgWidth = 208;
+      const imgHeight = canvas.height * imgWidth/canvas.width;
+      const imgData = canvas.toDataURL('img/png')
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+      pdf.save("mijn_canvas.pdf")
+    })
+  }
+
+  function drawLine({prevPoint, currentPoint, ctx}: Draw){
+    const {x: currx, y: currY} = currentPoint
+    const lineColor = color
+    const lineWidth = 5
+
+    let startPoint = prevPoint ?? currentPoint
+    ctx.beginPath()
+    ctx.lineWidth = lineWidth
+    ctx.strokeStyle = lineColor
+    ctx.moveTo(startPoint.x, startPoint.y)
+    ctx.lineTo(currx, currY)
+    ctx.stroke()
+    ctx.fillStyle = lineColor
+    ctx.beginPath()
+    ctx.arc(startPoint.x, startPoint.y,2,0,2* Math.PI)
+    ctx.fill()
+  }
+
+  const dropNote = event => {
+    event.target.style.left = `${event.pageX - 50}px`
+    event.target.style.top = `${event.pageY - 50}px`
+  }
+
+  const dragOver = event => {
+    event.stopPropagation()
+    event.preventDefault()
+  }
+
+
+  return(
+        <div onDragOver={dragOver} style={{backgroundColor: "#e17000"}}>
+
+          <form className="note-form " onSubmit={addNote} style={{position: "fixed", right: "15px", zIndex: 3, top: '30px'}}>
+                  <textarea placeholder="Create a new note..."
+                            value={noteInput}
+                            onChange={event => setNoteInput(event.target.value)}
+                            style={{paddingBottom: 0}}
+                  >
+                  </textarea>
+          <button
+              style={{height: '50px', width: '50px', textAlign: "center", top: 0, verticalAlign: "top", borderRadius: '3px', borderWidth: "3px", backgroundColor: "white", borderColor:"black"}}
+          >Add</button>
+
+
+        </form>
+          <button onClick={() => exportPdf()}><img src="/img/PDF_icon.svg.png"
+                                                   alt="clear"
+                                                   style={{ width: "108px", height: "107px" }}/></button>
+          <div style={{position: "fixed", right: "15px", zIndex: 3, top: '100px',display: 'flex', flexDirection: 'column', borderColor:"black",borderWidth:"3px"}}>
+          <button color={color} onClick={(e) => setColor('#000000')} className="color"><img
+              src="/img/black.png"
+              alt="black"
+          /></button>
+          <button color={color} onClick={(e) => setColor('#FF0000')} className="color"><img
+              src="/img/red.png"
+              alt="red"
+          /></button>
+          <button color={color} onClick={(e) => setColor('#0000FF')} className="color"><img
+              src="/img/blue.png"
+              alt="blue"
+          /></button>
+          <button color={color} onClick={(e) => setColor('#FFFF00')} className="color"><img
+              src="/img/yellow.png"
+              alt="yellow"
+          /></button>
+            <button onClick={clear}><img
+                src="/img/trash.png"
+                alt="clear"
+                style={{ width: "108px", height: "107px" }}
+            /></button>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <div id="App"
+        className="w-screen h-screen flex justify-center items-center"
+        style={{ position: "relative", backgroundColor: "#e17000" }}
+    >
+          {notesState
+            .notes
+            .map(note => (
+                <div className='note' draggable="true" onDragEnd={dropNote} key={note.id}>
+                  <div onClick={() => dispatch({type: 'DELETE_NOTE', payload: note})} className="close">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                    </svg>
+
+                  </div>
+
+                  <pre className="text">{note.text}</pre>
+                </div>
+            ))
+        }
+      <div style={{ position: "absolute", zIndex: 1 }}>
+        <canvas
+            onMouseDown={onMouseDown}
+            ref={canvasRef}
+            width={1246}
+            height={701}
+            className="border border-black rounded-md"
+        />
+      </div>
+      <div style={{ position: "absolute", pointerEvents: "none" }}>
+        <img
+            src="/img/prototypecanvas.PNG"
+            alt="Background"
+            style={{ width: "100%", height: "100%" }}
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+)
 }
+
+export default page
